b8eacdade8bf80d4d5ba1301ecebd97e710ad89d
Jacob-Hatanaka <jacobth@hawaii.edu>
2025-11-07 11:10:44 -1000

    notes-2 finished in 11 minutes.

--- src/components/ContactCardAdmin.tsx
+++ src/components/ContactCardAdmin.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Contact } from '@prisma/client';
-import { Card, Image } from 'react-bootstrap';
+import { Contact, Note } from '@prisma/client';
+import { Card, Image, ListGroup } from 'react-bootstrap';
+import NoteItem from './NoteItem';
 
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
-const ContactCardAdmin = ({ contact }: { contact: Contact }) => (
+const ContactCardAdmin = ({ contact, notes }: { contact: Contact; notes: Note[] }) => (
   <Card className="h-100">
     <Card.Header>
       <Image src={contact.image} width={75} />
@@ -13,16 +14,17 @@ const ContactCardAdmin = ({ contact }: { contact: Contact }) => (
         &nbsp;
         {contact.lastName}
       </Card.Title>
-      <Card.Subtitle>
-        {contact.address}
-      </Card.Subtitle>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
     </Card.Header>
     <Card.Body>
       {contact.description}
+      <ListGroup variant="flush">
+        {notes.map((note) => (
+          <NoteItem key={note.id} note={note} />
+        ))}
+      </ListGroup>
     </Card.Body>
-    <Card.Footer className="blockquote-footer">
-      {contact.owner}
-    </Card.Footer>
+    <Card.Footer className="blockquote-footer">{contact.owner}</Card.Footer>
   </Card>
 );
 export default ContactCardAdmin;
